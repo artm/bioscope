e797466ce842b9ea1418fe2cf8045a59115361bd
Artem Baguinski <artm@v2.nl>
2012-10-06 00:45:42 +0200

    ported more bits of file opening; passing the first test

--- Bioscope.cpp
+++ Bioscope.cpp
@@ -2,6 +2,7 @@
 
 struct Bioscope::Detail {
     static bool avInitialized;
+    AVFormatContext * formatContext;
 
     Detail() {
         if (!avInitialized) {
@@ -11,13 +12,47 @@ struct Bioscope::Detail {
     }
 };
 
+class LibAvLogger {
+public:
+    class Error {
+    public:
+        Error(const QString& msg) : m_message(msg) {}
+        QString message() { return m_message; }
+    private:
+        QString m_message;
+    };
+
+    void operator<<(int av_err) {
+        if (av_err) {
+            char buff[1024];
+            QString msg;
+
+            if (av_strerror(av_err, buff, sizeof(buff)) == 0)
+                qDebug() << (msg = buff);
+            else
+                qDebug() << (msg = "Unknown problem with ffmpeg libraries");
+
+            throw Error(msg);
+        }
+    }
+};
+
 bool Bioscope::Detail::avInitialized = false;
 
-Bioscope::Bioscope(const QString &, QObject *parent) :
+Bioscope::Bioscope(const QString & _path, QObject *parent) :
     QObject(parent),
     m_detail( new Detail )
 {
-    throw UnsupportedFile();
+    QString path = QFileInfo(_path).canonicalFilePath();
+
+    try {
+        LibAvLogger() << av_open_input_file( &m_detail->formatContext,
+                                             qPrintable(path), NULL, 0, NULL);
+        LibAvLogger() << av_find_stream_info( m_detail->formatContext );
+    } catch (LibAvLogger::Error) {
+        throw IOError();
+    }
+
 }
 
 Bioscope::~Bioscope()
@@ -31,9 +66,7 @@ bool Bioscope::supportedFile(const QString & path)
     try {
         Bioscope tmp(path);
         return true;
-    } catch (IOError&) {
-        return false;
-    } catch (UnsupportedFile&) {
+    } catch (...) {
         return false;
     }
 }
